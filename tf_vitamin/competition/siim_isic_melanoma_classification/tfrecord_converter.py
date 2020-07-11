1c15815e2f1df652fea01d640612ea2be554ad89
tomoyuki_s <>
2020-07-12 04:58:38 +0900

    fix missing arg.

--- tf_vitamin/competition/siim_isic_melanoma_classification/tfrecord_converter.py
+++ tf_vitamin/competition/siim_isic_melanoma_classification/tfrecord_converter.py
@@ -3,8 +3,8 @@ from tf_vitamin.dataset.tfrecord_converter import TfrecordConverter
 
 
 class TfrecordConverter(TfrecordConverter):
-    def __init__(self, filename, record_format):
-        super().__init__(filename, record_format)
+    def __init__(self, filename, record_format, image_size):
+        super().__init__(filename, record_format, image_size)
 
     def parse_train_example(self, example):
         example = tf.io.parse_single_example(example, self.record_format)
