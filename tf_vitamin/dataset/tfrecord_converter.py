352061e1a870944034dd28ba62b3d3fdc96009f5
tomoyuki_s <>
2020-07-12 04:54:03 +0900

    add images reshape

--- tf_vitamin/dataset/tfrecord_converter.py
+++ tf_vitamin/dataset/tfrecord_converter.py
@@ -1,6 +1,8 @@
+import re
 from abc import abstractmethod, ABCMeta
 from enum import Enum
 import tensorflow as tf
+import numpy as np
 
 AUTO = tf.data.experimental.AUTOTUNE
 
@@ -11,9 +13,10 @@ class ConvertMode(Enum):
 
 
 class TfrecordConverter(metaclass=ABCMeta):
-    def __init__(self, filename, record_format):
+    def __init__(self, filename, record_format, image_size):
         self.filename = filename
         self.record_format = record_format
+        self.image_size = image_size
 
     def convert2dataset(self, mode, ordered=False):
         dataset = tf.data.TFRecordDataset(
@@ -43,15 +46,20 @@ class TfrecordConverter(metaclass=ABCMeta):
     def parse_test_example(self, example):
         pass
 
-    @staticmethod
-    def decode_image(image_data):
+    def decode_image(self, image_data):
         image = tf.image.decode_jpeg(image_data, channels=3)
 
         # convert image to floats in [0, 1] range
         image = tf.cast(image, tf.float32) / 255.0
 
         # explicit size needed for TPU
-        # image = tf.reshape(image, [*self.orig_image_shape])
-        # image = tf.image.resize(image, [*self.reshape_size])
+        image = tf.reshape(image, [self.image_size, self.image_size])
+        image = tf.image.resize(image, [self.image_size, self.image_size, 3])
 
         return image
+
+
+def count_data_items(filenames):
+    # the number of data items is written in the name of the .tfrec files, i.e. flowers00-230.tfrec = 230 data items
+    n = [int(re.compile(r"-([0-9]*)\.").search(filename).group(1)) for filename in filenames]
+    return np.sum(n)
